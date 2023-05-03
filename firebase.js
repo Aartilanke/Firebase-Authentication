98618630ab3df84b8815e6ad864e1df29efe3487
Aarti Lanke <aarti.22020248@viit.ac.in>
2023-05-03 15:18:07 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,22 @@
+
+  // Import the functions you need from the SDKs you need
+  //import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+  //import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB4nsvlOeXKimqiafgZfwkHcEJpbvP_kH0",
+    authDomain: "authentication-ba5d0.firebaseapp.com",
+    projectId: "authentication-ba5d0",
+    storageBucket: "authentication-ba5d0.appspot.com",
+    messagingSenderId: "309336594541",
+    appId: "1:309336594541:web:aca6da473a2392b5bc4764",
+    measurementId: "G-000ZCSQE22"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
